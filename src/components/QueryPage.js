a4a0b46e0253ee2a85401631506114ffa767d7c3
Nigel Maynard <kahler.maynard@gmail.com>
2018-10-14 21:07:24 -0400

    queryPage structure

---
+++ src/components/QueryPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Delete from '@material-ui/icons/Delete';
+import Kitchen from '@material-ui/icons/Kitchen';
+
+import './QueryPage.scss';
+
+const QueryPage = props =>
+    <main className="QueryPage">
+        <section className="Selected">
+            <header className="Selected-Header">
+                <div className="Selected-Label">
+                    <Delete className="Selected-Icon" />
+                    <h1 className="Selected-Title">Pot</h1>
+                </div>
+                <button className="Selected-RecipesNav"></button>
+            </header>
+            <div className="Selected-Options">
+
+            </div>
+        </section>
+        <section className="Selector">"
+            <header className="Selector-Header">"
+                <div className="Selector-Label">
+                    <Kitchen className="Selector-Icon" />
+                    <h1 className="Selector-Title">Pantry</h1>
+                </div>
+            </header>
+            <div className="Selector-Options">
+
+            </div>
+        </section>
+    </main>
+
+export default QueryPage;
